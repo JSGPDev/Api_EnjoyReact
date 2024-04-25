db69baf8d7e2f7f145cf62bfe44cf7d1feb75c03
EnjoyPet <petenjoyyour@gmail.com>
2024-04-24 21:22:05 -0500

    fixing bugs

--- servicios/db.js
+++ servicios/db.js
@@ -5,10 +5,13 @@ const pool = mysql.createPool({
     user: process.env.DB_USER,
     password: process.env.DB_PASS,
     database: process.env.DB_NAME,
+    port: process.env.DB_PORT, // Añade esta línea si tu base de datos no usa el puerto 3306
     waitForConnections: true,
     connectionLimit: 10,
-    queueLimit: 0
+    queueLimit: 0,
+    connectTimeout: 30000 // Añade esta línea para un tiempo de espera de 30 segundos
 });
+
 
 pool.getConnection((err, connection) => {
     if (err) {
